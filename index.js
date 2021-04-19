078b24b9c941e7446f4abcdcc8663fb2a7cdd5fa
Martina Sierra <martinasierra94@gmail.com>
2021-04-19 16:54:15 -0300

    Update POST

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 4000;
 const users = require('./Users');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+//Register
+app.post('/register', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    fullName:req.body.fullName,
+    password:req.body.password,
+  };
+  if (!newUser.email || !newUser.fullName || !newUser.password) {
+    return res.status(400).json({ msg: 'Please include a name, email and password'});
+}
+else {
+    return res.json({result: 'Registration has been successful'});
+}
+});
+
+/*/ Login
+app.put('/login', (req, res) => {
+  const updateUser = req.body;
+  users.forEach(user => {
+    if (user.mail === parseInt(req.params.mail)) {
+
+    }
+  });
+  
+  res.json({result: 'Login has been succesfull'})
+}) */
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-function handleRegister() {
-
-}
-
-function handleLogin() {
-
-}
-
-app.post('/', (req, res) => {
-  const newUser = {
-    email: req.body.name,
-    fullName:req.body.fullName,
-    password:req.body.password,
-    status: 'active'
-  };
-
-  app.put('/', (req, res) => {
-    res.json()
-  })
-
-  users.push(newUser);
-  res.json(users);
-});
